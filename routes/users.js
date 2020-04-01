16f20417a86ec896bf3ef4cb93ec0fd6c8b9fe1c
Hanna <hanna-r-n@hotmail.com>
2020-04-01 13:26:31 +0200

    add task

--- routes/users.js
+++ routes/users.js
@@ -1,61 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const userHandler = require('../models/handleUsers');
+const userHandler = require("../models/handleUsers");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 //Register
-router.get('/register', function(req, res) {
-    // display register route
-    res.render('register', {
-        // display register form view
-        title: 'Register User' // input data to view
-    });
+router.get("/register", function(req, res) {
+  // display register route
+  res.render("register", {
+    // display register form view
+    title: "Register User" // input data to view
+  });
 });
-router.post('/register', function(req, res) {
-    // new user post route
-    userHandler.upsertUser(req);
-    return res.redirect('/'); // skip the receipt, return to fp
+router.post("/register", function(req, res) {
+  // new user post route
+  userHandler.upsertUser(req);
+  return res.redirect("/"); // skip the receipt, return to fp
 });
 //Login
-router.get('/login', function(req, res) {
-    // display register route
-    res.render('login', {
-        // display register form view
-        title: 'User Login', // input data to view
-        message: ''
-    });
+router.get("/login", function(req, res) {
+  // display register route
+  res.render("login", {
+    // display register form view
+    title: "User Login", // input data to view
+    message: ""
+  });
 });
-router.post('/login', async function(req, res) {
-    // new user post route
-    let rc = await userHandler.verifyUser(req); // verify credentials
-    if (rc) {
-        res.render('toDos', {
-            // find the view 'index'
-            title: 'Your Todos', // input data to 'index'
-            loggedin: true,
-            message: 'Logged in as',
-            who: req.session.user // using session var(s)
-            task: task
-        });
-    } else {
-        res.render('login', {
-            // find the view 'login'
-            title: 'User Login', // input data to 'login'
-            message: 'The username or password is incorrect. Try again',
-            loggedin: false
-        });
-    }
+router.post("/login", async function(req, res) {
+  // new user post route
+  let rc = await userHandler.verifyUser(req); // verify credentials
+  if (rc) {
+    res.render("toDos", {
+      // find the view 'index'
+      title: "Your Todos", // input data to 'index'
+      loggedin: true,
+      message: "Logged in as",
+      who: req.session.user, // using session var(s)
+      task: task
+    });
+  } else {
+    res.render("login", {
+      // find the view 'login'
+      title: "User Login", // input data to 'login'
+      message: "The username or password is incorrect. Try again",
+      loggedin: false
+    });
+  }
 });
 //To Dos
-router.get('/toDos', function(req, res) {
-    // display register route
-    res.render('toDos', {
-        // display register form view
-        title: 'Your Todos' // input data to view
-    });
+router.get("/toDos", function(req, res) {
+  // display register route
+  res.render("toDos", {
+    // display register form view
+    title: "Your Todos" // input data to view
+  });
 });
 
 var task = ["buy socks", "practise with nodejs"];
@@ -68,30 +68,29 @@ router.post("/login", function(req, res) {
   res.redirect("/");
 });
 
-
 //Admin
-router.get('/admin', async function(req, res) {
-    let rc = await userHandler.getUsers(req); // verify credentials
-    if (rc) {
-        res.render('admin', {
-            // find the view 'index'
-            title: 'Admin Panel', // input data to 'index'
-            loggedin: true,
-            message: 'Logged in as Admin',
-            who: req.session.user // using session var(s)
-        });
-    } else {
-        res.render('login', {
-            // find the view 'login'
-            title: 'User Login', // input data to 'login'
-            message: 'The username or password is incorrect. Try again',
-            loggedin: false
-        });
-    }
+router.get("/admin", async function(req, res) {
+  let rc = await userHandler.getUsers(req); // verify credentials
+  if (rc) {
+    res.render("admin", {
+      // find the view 'index'
+      title: "Admin Panel", // input data to 'index'
+      loggedin: true,
+      message: "Logged in as Admin",
+      who: req.session.user // using session var(s)
+    });
+  } else {
+    res.render("login", {
+      // find the view 'login'
+      title: "User Login", // input data to 'login'
+      message: "The username or password is incorrect. Try again",
+      loggedin: false
+    });
+  }
 });
-router.get('/admin/:user', async function(req, res) {
-    let user = await userHandler.getUsers({}, { sort: { name: 1 } });
-    res.json(user);
+router.get("/admin/:user", async function(req, res) {
+  let user = await userHandler.getUsers({}, { sort: { name: 1 } });
+  res.json(user);
 });
 
 module.exports = router;
