e012f25f95ac4c089475c2e6ba2776983337e24e
Awienberg <a.wienberg@hotmail.com>
2020-04-01 13:14:19 +0200

    handleToDos

---
+++ models/handleToDos.js
@@ -0,0 +1,38 @@
+"use strict";
+const mon = require("./mongooseWrap");
+const ToDos = require("./ToDos");
+const dbServer = "localhost";
+const dbname = "Todos";
+
+exports.getToDos = async function (query, sort) {
+    try {
+        let cs = await mon.retrieve(dbServer, dbname, ToDos, query, sort);
+        return cs;
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+exports.upsertToDos = async function (req) {
+    let check = { title: req.body.title };
+    let ToDos = new ToDos({
+        title: req.body.title,
+        todo: req.body.todo,
+        created: req.body.created,
+        deadline: req.body.deadline,
+    });
+    try {
+        let cs = await mon.upsert(dbServer, dbname, ToDos, ToDos, check);
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+exports.deleteToDos = async function (name) {
+    try {
+        let cs = await mon.remove(dbServer, dbname, ToDos, name);
+        return cs;
+    } catch (e) {
+        console.log(e);
+    }
+}
