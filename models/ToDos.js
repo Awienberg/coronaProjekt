6a3906f9cac3867b6cd9017aea2e56f9ed62b4c5
Awienberg <a.wienberg@hotmail.com>
2020-04-01 13:12:30 +0200

    handleToDos

--- models/ToDos.js
+++ models/ToDos.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const toDosSchema = mongoose.Schema({
-    userID: {
-        type: String,
-        required: true
-    },
     title: {
         type: String,
         required: true
