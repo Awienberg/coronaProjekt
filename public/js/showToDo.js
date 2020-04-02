57dea03c5471492a876b82bd269f1f20b2a82993
Hanna <hanna-r-n@hotmail.com>
2020-04-02 11:44:22 +0200

    ShowTodo

--- public/js/showToDo.js
+++ public/js/showToDo.js
@@ -2,102 +2,79 @@
 import { $ } from "./modules/nQuery.js";
 import { Ajax } from "./modules/Ajax.js";
 
-const getUsers = function(ev) {
+const getToDos = function(ev) {
   //continents
   let req = Object.create(Ajax);
   req.init();
-  req.getFile("/users/admin/:user", showUsers);
+  req.getFile("/users/toDos/:todo", showToDo);
 };
 //Show users
-const showUsers = function(e) {
+const showToDo = function(e) {
   console.log(e.target.getResponseHeader("Content-Type"));
-  let element = $("users");
+  let element = $("addTask");
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
-  let users = JSON.parse(e.target.responseText);
+  let todos = JSON.parse(e.target.responseText);
   let tabel = document.createElement("table");
   let tr = document.createElement("tr");
   let th = document.createElement("th");
-  let p = document.createTextNode("User ID");
+  let p = document.createTextNode("Task");
   th.appendChild(p);
   tr.appendChild(th);
 
   let th1 = document.createElement("th");
-  let p1 = document.createTextNode("Firstname");
+  let p1 = document.createTextNode("Title");
   th1.appendChild(p1);
   tr.appendChild(th1);
 
   let th2 = document.createElement("th");
-  let p2 = document.createTextNode("Lastname");
+  let p2 = document.createTextNode("Todo");
   th2.appendChild(p2);
   tr.appendChild(th2);
 
   let th3 = document.createElement("th");
-  let p3 = document.createTextNode("Edit");
+  let p3 = document.createTextNode("created");
   th3.appendChild(p3);
   tr.appendChild(th3);
 
   let th4 = document.createElement("th");
-  let p4 = document.createTextNode("delete");
+  let p4 = document.createTextNode("deadline");
   th4.appendChild(p4);
   tr.appendChild(th4);
 
   tabel.appendChild(tr);
 
-  users.forEach(function(user) {
+  todos.forEach(function(todo) {
     let tr1 = document.createElement("tr");
 
     let td1 = document.createElement("td");
-    let id = document.createTextNode(user.userID);
+    let id = document.createTextNode(todo.title);
     td1.appendChild(id);
     tr1.appendChild(td1);
 
     let td2 = document.createElement("td");
-    let fn = document.createTextNode(user.firstName);
+    let fn = document.createTextNode(todo.todo);
     td2.appendChild(fn);
     tr1.appendChild(td2);
 
     let td3 = document.createElement("td");
-    let ln = document.createTextNode(user.lastName);
+    let ln = document.createTextNode(todo.created);
     td3.appendChild(ln);
     tr1.appendChild(td3);
 
     let td4 = document.createElement("td");
-    let btn = document.createElement("button");
-    btn.setAttribute("class", "btn");
-    let edit = document.createTextNode("Edit");
-    td4.appendChild(btn);
-    btn.appendChild(edit);
+    let deadline = document.createElement(todo.deadline);
+    td4.appendChild(deadline);
     tr1.appendChild(td4);
-
-    let td5 = document.createElement("td");
-    let form = document.createElement("form");
-    form.setAttribute("method", "POST");
-    form.setAttribute("action", "/users/admin/:user");
-
-    let input = document.createElement("input");
-    input.setAttribute("value", user.userID);
-    input.setAttribute("name", "userID");
-    input.setAttribute("type", "hidden");
-
-    let delB = document.createElement("button");
-    let deltext = document.createTextNode("Delete");
-    delB.setAttribute("class", "del");
-
-    form.appendChild(input);
-    form.appendChild(delB);
-    delB.appendChild(deltext);
-    td5.appendChild(form);
-    tr1.appendChild(td5);
     tabel.appendChild(tr1);
   });
 
-  $("users").appendChild(tabel);
+  $("addTask").appendChild(tabel);
 };
 
 function showstarter() {
-  getUsers();
+  getToDos();
 }
 
-window.addEventListener("load", showstarter); // kick off JS
+window.addEventListener("load", showstarter);
