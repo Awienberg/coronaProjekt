bedfdadb3afbb20b897673baf35bc609784968b9
mads <mads2097@gmail.com>
2020-04-03 13:27:00 +0200

    nvm shit is still broken

--- public/js/admin.js
+++ public/js/admin.js
@@ -81,12 +81,12 @@ const showUsers = function(e) {
         let input1 = document.createElement('input');
         input1.setAttribute('value', user.userID);
         input1.setAttribute('name', 'userID');
-        input1.setAttribute('type', 'text');
+        input1.setAttribute('type', 'hidden');
 
         let input2 = document.createElement('input');
         input2.setAttribute('value', user.isApproved);
-        input2.setAttribute('name', 'userID');
-        input2.setAttribute('type', 'text');
+        input2.setAttribute('name', 'approve');
+        input2.setAttribute('type', 'hidden');
 
         let appB = document.createElement('button');
         // let deltext = document.createTextNode(' Delete');
