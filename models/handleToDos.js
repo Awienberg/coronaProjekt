8fff88c3792f1b207c9e452a6970470e4b733406
Hanna <hanna-r-n@hotmail.com>
2020-04-03 12:18:30 +0200

    edit knap virker ikke

--- models/handleToDos.js
+++ models/handleToDos.js
@@ -4,35 +4,50 @@ const ToDos = require("./ToDos");
 const dbServer = "localhost";
 const dbname = "Todos";
 
-exports.getToDos = async function (query, sort) {
-    try {
-        let cs = await mon.retrieve(dbServer, dbname, ToDos, query, sort);
-        return cs;
-    } catch (e) {
-        console.error(e);
-    }
+exports.getToDos = async function(query, sort) {
+  try {
+    let cs = await mon.retrieve(dbServer, dbname, ToDos, query, sort);
+    return cs;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-exports.upsertToDos = async function (req) {
-    let check = { title: req.body.title };
-    let todo = new ToDos({
-        title: req.body.title,
-        todo: req.body.todo,
-        created: req.body.created,
-        deadline: req.body.deadline,
-    });
-    try {
-        let cs = await mon.upsert(dbServer, dbname, ToDos, todo, check);
-    } catch(e) {
-        console.error(e);
-    }
+exports.upsertToDos = async function(req) {
+  let check = { title: req.body.title };
+  let todo = new ToDos({
+    title: req.body.title,
+    todo: req.body.todo,
+    created: req.body.created,
+    deadline: req.body.deadline
+  });
+  try {
+    let cs = await mon.upsert(dbServer, dbname, ToDos, todo, check);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-exports.deleteToDos = async function (name) {
-    try {
-        let cs = await mon.remove(dbServer, dbname, ToDos, name);
-        return cs;
-    } catch (e) {
-        console.log(e);
-    }
-}
+exports.changeToDos = async function(req) {
+  let check = { title: req.body.title };
+  let todo = new ToDos({
+    title: req.body.title,
+    todo: req.body.todo,
+    created: req.body.created,
+    deadline: req.body.deadline
+  });
+  try {
+    let cs = await mon.upsert(dbServer, dbname, ToDos, todo, check);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+exports.deleteToDos = async function(name) {
+  try {
+    let cs = await mon.remove(dbServer, dbname, ToDos, name);
+    return cs;
+  } catch (e) {
+    console.log(e);
+  }
+};
